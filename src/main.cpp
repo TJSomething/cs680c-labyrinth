8215c23f5f30e9b61b31f640c6ae52d6be43a8e2
Thomas Kelly <tjbk123@gmail.com>
2012-10-08 05:10:36 -0700

    Zoomed in on the board

--- src/main.cpp
+++ src/main.cpp
@@ -921,7 +921,7 @@ bool initialize()
     //  if you will be having a moving camera the view matrix will need to more dynamic
     //  ...Like you should update it before you render more dynamic 
     //  for this project having them static will be fine
-    mats::view = glm::lookAt( glm::vec3(0.0, 24.0, -24.0), //Eye Position
+    mats::view = glm::lookAt( glm::vec3(0.0, 18.0, -18.0), //Eye Position
                         glm::vec3(0.0, 0.0, 0.0), //Focus point
                         glm::vec3(0.0, 1.0, 0.0)); //Positive Y is up
 
